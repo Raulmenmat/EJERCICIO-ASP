ac001cbe76c36c8a01d2fa4c10dcc7d3323ce614
Raulmenmat <raulmenmat@monlau.com>
2022-03-15 19:38:34 +0100

    first commit

---
+++ Web01/Web01/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Net.Mail;
+using Web01.Models;
+
+namespace Web01.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult metodoEmail(ClassEmail classEmail)
+        {
+            email(classEmail);
+            
+            return View("Index");
+        }
+
+        private void email(ClassEmail classEmail)
+        {
+          MailMessage mail =new MailMessage();
+
+
+            mail.To.Add(classEmail.email);
+
+            mail.From= new MailAddress("ggerardab@gmail.com");
+            mail.Subject=classEmail.asunto;
+            mail.Body = classEmail.mensaje;
+
+
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new System.Net.NetworkCredential("ggerardab@gmail.com","2Barcelona$$");
+            smtp.EnableSsl = true;
+            smtp.Send(mail);
+
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Web01/Web01/Models/ClassEmail.cs
@@ -0,0 +1,12 @@
+﻿namespace Web01.Models
+{
+    public class ClassEmail
+    {
+
+       
+        public String email { get; set; }
+        public String asunto { get; set; }
+        public String mensaje { get; set; }
+
+    }
+}
